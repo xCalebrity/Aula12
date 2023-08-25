2f3fe49be62a76ab86f4d94076087ad141264a85
tainami <tainadarosa10@gmail.com>
2023-08-24 21:50:16 -0300

    adicionado respostas parciais do exercício da aula 12

---
+++ template_javascript/index.js
@@ -0,0 +1,39 @@
+/*
+Interpretação de código
+=======================
+
+(1)
+function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10))
+
+====================
+(a)
+
+o primeiro console log irá imprimir 10, e o segundo 50
+
+===================
+(b)
+
+
+
+
+===================
+(2)
+
+let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)
+
+=====================
+(a)
+
+*/
